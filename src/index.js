0f06c0d7cf4b0f136136e546a02522a90c48d721
Morales <gabriel.morales@thevelops.com>
2018-03-08 14:12:20 -0300

    [master] fixed route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import smoothscroll from "smoothscroll";
 
-
 // Controlling language select
 const languageSelect = document.querySelector("[lg-select]");
 languageSelect.addEventListener("click", e => {
